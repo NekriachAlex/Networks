82e42e27de5e95fdc28d430a74457782d3929b51
Alex <nekryachalex1@yandex.ru>
2024-02-27 21:02:07 +0700

    reduced prog

--- Simple_proxy/server.c
+++ Simple_proxy/server.c
@@ -11,17 +11,9 @@
 #define MAX_CLIENTS 5
 #define BUFFER_SIZE 512
 
-void handle_client(int client_socket, int target_socket)
- {
-    char buffer[BUFFER_SIZE];
-    int bytes_read;
-
-    while ((bytes_read = read(client_socket, buffer, sizeof(buffer))) > 0) {
-        write(target_socket, buffer, bytes_read);
-    }
-
-    close(client_socket);
-    close(target_socket);
+void handle_client()
+{
+    return;    
 }
 
 int main()
@@ -38,9 +30,9 @@ int main()
     struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(struct sockaddr_in));
     server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
     
-
- 
     if(bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address)) == -1)
     {
         perror("Cant bind server socket");
@@ -67,26 +59,24 @@ int main()
     int serverIsFull = 0;
     while(1)
     {
-        if(poll((struct pollfd *)&serverFds[serverIsFull], currentSockets, -1) == -1)
+        if(poll((struct pollfd *)&server_fds[serverIsFull], currentSockets, -1) == -1)
         {
             perror("Cant using poll");
-            unlink(PATH_TO_SOCKET);
             exit(1);
         }
 
-        if((serverFds[0].revents & POLLIN) && (serverIsFull == 0)) 
+        if((server_fds[0].revents & POLLIN) && (serverIsFull == 0)) 
         {
             int clientFd;
-            if((clientFd = accept(serverSocket, NULL, NULL)) == -1)
+            if((clientFd = accept(server_socket, NULL, NULL)) == -1)
             {
                 perror("Cant accept");
-                unlink(PATH_TO_SOCKET);
                 exit(1);
             }
             clientCounter++;
-            serverFds[clientCounter].fd = clientFd;
+            server_fds[clientCounter].fd = clientFd;
             currentSockets++;
-            if(clientCounter == NUMBER_OF_CLIENTS)
+            if(clientCounter == MAX_CLIENTS)
             {
                 serverIsFull = 1;
             }
@@ -94,23 +84,17 @@ int main()
 
         for(nfds_t clientSocketNumber = 1; clientSocketNumber <= clientCounter; clientSocketNumber++)
         {
-            if(serverFds[clientSocketNumber].revents & POLLIN)
+            if(server_fds[clientSocketNumber].revents & POLLIN)
             {
-                size_t readSize = read(serverFds[clientSocketNumber].fd, buffer, BUFFER_SIZE);
                    
-                if(readSize > 0)
-                {
-                    printUpper(buffer, readSize);
-                }
-                else
-                {
-                    close(serverFds[clientSocketNumber].fd);
-                    serverFds[clientSocketNumber].fd = serverFds[clientCounter].fd;
-                    serverFds[clientCounter].fd = -1;
+                    handle_client();
+                    close(server_fds[clientSocketNumber].fd);
+                    server_fds[clientSocketNumber].fd = server_fds[clientCounter].fd;
+                    server_fds[clientCounter].fd = -1;
                     clientCounter--;
                     currentSockets--;
                     serverIsFull = 0;
-                }
+                
             }
 
         }
